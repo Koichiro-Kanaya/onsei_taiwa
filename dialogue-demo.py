0011d82b0b2e19d7aba38f0f277f311164b60224
Koichiro-Kanaya <abcs516tk1@gmail.com>
2019-12-05 16:47:18 +0900

    インスタンス生成のタイミングの変更

--- dialogue-demo.py
+++ dialogue-demo.py
@@ -2,16 +2,25 @@
 # 音声対話システムデモ
 
 # めも
-# ・asrresult.txtは自分が言った内容が入っている。(ex.今日のあれは？
+# ・asrresult.txtは自分が言った内容が入っている。(ex.今日のあれは？)
 
 import os
 import subprocess
 import sys
+from response import response_gen
+from onsei_taiwa_lib.angry_siri import AngrySiri
 
-# 悪い言葉リスト
-nega_wordslist = ['ゴミ', 'カス']
-# 良い言葉リスト
-posi_wordslist = ['ありがとう', '感謝']
+
+# 悪い言葉リストの作成
+nega_wordslist = []
+with open('./nega_word.txt') as f:
+    for line in f:
+        nega_wordslist.append(line.rstrip('\n'))
+# 良い言葉リストの作成
+posi_wordslist = []
+with open('./posi_word.txt') as f:
+    for line in f:
+        posi_wordslist.append(line.rstrip('\n'))
 
 # dialogue directory
 tmpdirname = './tmp/dialogue'
@@ -23,62 +32,77 @@ if os.path.isdir(tmpdirname) == False:
 
 
 def main():
-    while True:
-        # inputファイルを定義
-        filename = tmpdirname + '/input.wav'
-        subprocess.call('adinrec ' + filename + ' > /dev/null', shell=True)
+    # インスタンスの初期化
+    siri = AngrySiri()
 
-        #話者認識
-        sidfile = tmpdirname + '/spkid.txt'
-        subprocess.call('touch ' + sidfile, shell=True)
-        subprocess.call('./sid/test.sh ' + filename + ' '+ sidfile, shell=True)
+    # 初期化メッセージを送信し、その返答を表示
+    siri.init_talk() # print(siri.init_talk())
+    
+    try:
+        while True:
+            # inputファイルを定義
+            filename = tmpdirname + '/input.wav'
+            subprocess.call('adinrec ' + filename + ' > /dev/null', shell=True)
 
-        # 現在の話者番号を取得
-        # もし前の状態を保存しておきたければ、別変数/別ファイルを用意する
-        with open(sidfile, 'r') as f:
-            sidnum = f.read().strip('\n')
+            #話者認識
+            sidfile = tmpdirname + '/spkid.txt'
+            subprocess.call('touch ' + sidfile, shell=True)
+            subprocess.call('./sid/test.sh ' + filename + ' '+ sidfile, shell=True)
 
-        # 音声認識
-        asrresult = tmpdirname + '/asrresult.txt'
-        with open(tmpdirname + '/list.txt', mode='w') as f:
-            f.write(filename)
+            # 現在の話者番号を取得
+            # もし前の状態を保存しておきたければ、別変数/別ファイルを用意する
+            with open(sidfile, 'r') as f:
+                sidnum = f.read().strip('\n')
 
-        # 音声認識をして結果をファイルに保存
-	    # もし前の状態を保存しておきたければ別変数/別ファイルを用意する
-        subprocess.call('julius -C asr/grammar.jconf -filelist ' + tmpdirname + '/list.txt 2> /dev/null | grep "^sentence1: " | sed -e "s/sentence1://" -e "s/silB//" -e "s/silE//" -e "s/ //g" > ' + asrresult, shell = True)
-        subprocess.call('rm ./' + tmpdirname + '/list.txt', shell = True)
+            print("あなたは" + sidnum + "さんですね！")
 
+            # 音声認識
+            asrresult = tmpdirname + '/asrresult.txt'
+            with open(tmpdirname + '/list.txt', mode='w') as f:
+                f.write(filename)
 
-        # 自分の発言にnega/posi発言が何個ずつあるかを取得する。
-        nega_cnt = 0
-        posi_cnt = 0
-        with open(asrresult, 'r') as f:
-            sentence = f.read().strip('\n')
-        # 悪い言葉をカウント
-        for index in range(len(nega_wordslist)):
-            if nega_wordslist[index] in sentence:
-                nega_cnt += 1
-        # いい言葉をカウント
-        for index in range(len(posi_wordslist)):
-            if posi_wordslist[index] in sentence:
-                posi_cnt += 1
+            # 音声認識をして結果をファイルに保存
+            # もし前の状態を保存しておきたければ別変数/別ファイルを用意する
+            subprocess.call('julius -C asr/grammar.jconf -filelist ' + tmpdirname + '/list.txt 2> /dev/null | grep "^sentence1: " | sed -e "s/sentence1://" -e "s/silB//" -e "s/silE//" -e "s/ //g" > ' + asrresult, shell = True)
+            subprocess.call('rm ./' + tmpdirname + '/list.txt', shell = True)
 
 
-        # 話者認識/音声認識結果を応答を生成する
-        # 状態/履歴への依存性を持たせたければこのプログラムを適宜修正（引数変更等）
-        # 初期では話者ID を元に異なる応答リストを読み込む仕様
-        subprocess.call('./response.py ' + str(nega_cnt) + ' ' + str(posi_cnt) + ' ' + asrresult, shell = True)
-
-
-        #事後処理
-        subprocess.call('rm ' + filename + ' ' + sidfile + ' ' + asrresult, shell = True)
-
+            # 自分の発言にnega/posi発言が何個ずつあるかを取得する。
+            nega_cnt = 0
+            posi_cnt = 0
+            with open(asrresult, 'r') as f:
+                sentence = f.read().strip('\n')
+                # ユーザの言葉を表示する
+                print('あなた : ' + sentence)
         
 
-# Ctrl-C で抜けるための処理
-try:
-    main()
-except KeyboardInterrupt:
-    os.system('rm -r ' + tmpdirname)
-    os.system('rm -r ./tmp')
-    sys.exit(0)
+            # 悪い言葉をカウント
+            for index in range(len(nega_wordslist)):
+                if nega_wordslist[index] in sentence:
+                    nega_cnt += 1
+            # いい言葉をカウント
+            for index in range(len(posi_wordslist)):
+                if posi_wordslist[index] in sentence:
+                    posi_cnt += 1
+
+            # 話者認識/音声認識結果を応答を生成する
+            # 状態/履歴への依存性を持たせたければこのプログラムを適宜修正（引数変更等）
+            # 話者ID を元に異なる応答リストを読み込む仕様
+
+            # 認識結果
+            with open(asrresult, 'r') as f: # ユーザの発言内容
+                question = f.read()
+            response_gen(siri, question, nega_cnt, posi_cnt, sidnum)
+
+
+            #事後処理
+            subprocess.call('rm ' + filename + ' ' + sidfile + ' ' + asrresult, shell = True)
+
+    # Ctrl-C で抜けるための処理
+    except KeyboardInterrupt:
+        os.system('rm -r ' + tmpdirname)
+        os.system('rm -r ./tmp')
+        sys.exit(0)
+
+
+main()
--- response.py
+++ response.py
@@ -5,46 +5,34 @@
 # 基本的には
 # - (str)nega_cnt (argv[1])
 # - (str)posi_cnt (argv[2])
-# - 音声認識結果が入っているファイルのパス (argv[3])
+# - 発言した人の番号 (argv[3])
+# - 音声認識結果が入っているファイルのパス (argv[4])
 # を受け取って応答文および音声を生成する
 #
 # 前の応答への依存性を持たせたい場合は引数を追加すれば良い
 import sys, os, subprocess
-# import
-from onsei_taiwa_lib.angry_siri import AngrySiri
 
 # 音声合成エンジンのpath
 #jtalkbin = '/usr/local/open_jtalk-1.07/bin/open_jtalk '
 #options = ' -m syn/nitech_jp_atr503_m001.htsvoice -ow /tmp/dialogue/out.wav -x /usr/local/open_jtalk-1.07/dic'
 
 jtalkbin = 'open_jtalk '
-options = '-m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice -ow /tmp/dialogue/out.wav -x /var/lib/mecab/dic/open-jtalk/naist-jdic'
+options = '-m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice -ow ./tmp/dialogue/out.wav -x /var/lib/mecab/dic/open-jtalk/naist-jdic'
 
-# 音声合成のコマンドを生成 (open jtalk を 使う場合
+# 音声合成のコマンドを生成
 def mk_jtalk_command(answer):
     jtalk = 'echo "' + answer + '" | ' + jtalkbin + options + ';'
-    play = 'play -q /tmp/dialogue/out.wav; rm /tmp/dialogue/out.wav;'
+    play = 'play -q ./tmp/dialogue/out.wav; rm ./tmp/dialogue/out.wav;'
     return jtalk + play
 
-if __name__ == '__main__':
-    nega_cnt = int(sys.argv[1])
-    posi_cnt = int(sys.argv[2])
-
-    # 認識結果
-    asrresult = open(sys.argv[3],'r') # sys.argv[3]には、ユーザの発言内容が入っている。
-    question = asrresult.read().rstrip() # ユーザの発言内容
-    asrresult.close()
-
-
-    # インスタンスの初期化
-    siri = AngrySiri()
-
-    # 初期化メッセージを送信し、その返答を表示
-    siri.init_talk() # print(siri.init_talk())
-
+def response_gen(siri, question, nega_cnt, posi_cnt, sidnum):
     # カスというメッセージを送信し、angryポイントを1加算し、その返答を表示
-    answer, answer_ = siri.talk(question, happy_cnt=posi_cnt, angry_cnt=nega_cnt)
-    print(answer) # => ('せやな！', None)
+    answer, _ = siri.talk(question, happy_cnt=posi_cnt, angry_cnt=nega_cnt)
+    print('Siri   : ' + answer) # => ('せやな！', None)
+    print('-------------------------')
+    print(siri.score)
+    print(posi_cnt)
+    print(nega_cnt)
 
     os.system(mk_jtalk_command(answer))
 
